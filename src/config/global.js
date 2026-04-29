dfc0b0806fc819194ba697e4813bc6587a108236
cveimarsena <vcelis@sena.edu.co>
2026-04-29 04:45:00 -0500

    avance

--- src/config/global.js
+++ src/config/global.js
@@ -1,6 +1,6 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
+    Name: 'Fundamentos del SARLAFT',
     Description: 'Descripción del RED',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
