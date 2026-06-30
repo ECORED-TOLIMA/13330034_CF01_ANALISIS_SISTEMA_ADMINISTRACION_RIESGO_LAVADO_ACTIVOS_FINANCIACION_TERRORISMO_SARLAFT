45740b87be0586deb5567d109a0de58c9431273f
cveimarsena <vcelis@sena.edu.co>
2026-06-30 11:14:44 -0500

    Empaquetado

--- src/config/global.js
+++ src/config/global.js
@@ -345,7 +345,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Profesional G06. Responsable Ecosistema de Recursos Educativos Digitales',
+            'Profesional G06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
           centro: 'Centro Agroturistico - Regional Santander',
         },
         {
